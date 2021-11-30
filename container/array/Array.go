d4f7f38a497677e67fc62f7d23f40a7280669c8f
weidongge <weidongge@51shebao.com>
2021-11-30 16:36:31 +0800

    array

--- container/array/Array.go
+++ container/array/Array.go
@@ -27,21 +27,30 @@ func main() {
 	makeSlice()
 	//copy
 	copySlice()
+	//remove array
+	removeArray(arr)
+}
+
+func removeArray(arr [10]string) {
+	remIdx := 2
+	var arr2 []string
+	arr2 = append(arr[:remIdx], arr[remIdx+1:]...)
+	fmt.Println("remove ", arr2)
 }
 
 //copy()实现往切片复制
 func copySlice() {
-	c1 := []int{1,2,3}
-	c2 := []int{4,5}
+	c1 := []int{1, 2, 3}
+	c2 := []int{4, 5}
 	copy(c2, c1)
 	fmt.Println(c1[:], c2[:])
 
-	c3 := []int{1,2,3}
-	c4 := []int{4,5}
+	c3 := []int{1, 2, 3}
+	c4 := []int{4, 5}
 	copy(c3, c4)
 	fmt.Println(c3[:], c4[:])
 
-	c5 := []int{9,10}
+	c5 := []int{9, 10}
 	//将c5引用给refC6   c5变动 refC6也变动
 	refC6 := c5
 	refC7 := make([]int, 3)
@@ -58,32 +67,31 @@ func appendSlice() {
 	strList = append(strList, 4, 5, 6)
 	fmt.Println(strList)
 	//追加一个切片
-	strList = append(strList, []int{7,8,9}...)
+	strList = append(strList, []int{7, 8, 9}...)
 	fmt.Println(strList)
 	//头部追加
-	strList = append([]int{11,12,13}, strList...)
+	strList = append([]int{11, 12, 13}, strList...)
 	fmt.Println(strList)
 }
 
 //make()函数构建切片 make( []Type, size, cap )  size:分配元素数  cap:为预分配的元素数量，这个值设定后不影响 size，只是能提前分配空间，降低多次分配空间造成的性能问题。
-func makeSlice(){
+func makeSlice() {
 	strList := make([]int, 2, 3)
 	fmt.Println(strList[:], len(strList))
 }
 
 //切片就是一个数组子集  通过下标截取一定位置的元素
-func sliceSplit(arr [10]string)  {
+func sliceSplit(arr [10]string) {
 	//输出从0 到数组长度的所有数据
-	fmt.Println(arr[0 : len(arr)])
+	fmt.Println(arr[0:len(arr)])
 	//省略结束位置 默认数组长度结束  同理省略开始位置
-	fmt.Println(arr[0 : ])
+	fmt.Println(arr[0:])
 	fmt.Println(arr[:])
 	//截取1-5
 	fmt.Println(arr[1:5])
 }
 
-
-func normalFor(arr [10]string)  {
+func normalFor(arr [10]string) {
 	//增强for循环遍历
 	for index, value := range arr {
 		fmt.Printf("arr index %d item %s", index, value)
